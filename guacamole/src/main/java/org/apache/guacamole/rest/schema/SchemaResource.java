08bfd94c3c90053a563515e4e3d48c648148d918
Nick Couchman <vnick@apache.org>
2017-08-02 17:43:09 -0400

    GUACAMOLE-221: Add REST endpoint for retrieving ProtocolInfo for specific protocol

--- guacamole/src/main/java/org/apache/guacamole/rest/schema/SchemaResource.java
+++ guacamole/src/main/java/org/apache/guacamole/rest/schema/SchemaResource.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.apache.guacamole.GuacamoleException;
@@ -154,6 +155,25 @@ public class SchemaResource {
         // Get and return a map of all protocols.
         Environment env = new LocalEnvironment();
         return env.getProtocols();
+
+    }
+
+    /**
+     * Get the ProtocolInfo for a specific protocol.
+     *
+     * @return
+     *     A ProtocolInfo object for the specified protocol.
+     *
+     * @throws GuacamoleException
+     *     If an error occurs while retrieving the protocol.
+     */
+    @GET
+    @Path("protocol/{protocol}")
+    public ProtocolInfo getProtocol(@PathParam("protocol") String protocol)
+            throws GuacamoleException {
+
+        Environment env = new LocalEnvironment();
+        return env.getProtocol(protocol);
 
     }
 
