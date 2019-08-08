acf993ef14b448fa1100a8b8f6581d1597f20e73
Virtually Nick <vnick@apache.org>
2019-08-08 16:21:44 -0400

    GUACAMOLE-860: [WIP] More tn5250 work.

--- guacamole-ext/src/main/java/org/apache/guacamole/environment/LocalEnvironment.java
+++ guacamole-ext/src/main/java/org/apache/guacamole/environment/LocalEnvironment.java
@@ -53,7 +53,7 @@ public class LocalEnvironment implements Environment {
      * Array of all known protocol names.
      */
     private static final String[] KNOWN_PROTOCOLS = new String[]{
-        "vnc", "rdp", "ssh", "telnet", "kubernetes"};
+        "vnc", "rdp", "ssh", "telnet", "kubernetes", "tn5250"};
 
     /**
      * The hostname to use when connecting to guacd if no hostname is provided
