b1f35c28f442bea7a3c908f4a23de8bf0a06edec
Virtually Nick <vnick@apache.org>
2020-07-03 19:48:13 -0400

    GUACAMOLE-1123: Add new REST API endpoints for retrieving historical data.

--- guacamole/src/main/java/org/apache/guacamole/rest/history/HistoryResource.java
+++ guacamole/src/main/java/org/apache/guacamole/rest/history/HistoryResource.java
@@ -21,6 +21,7 @@ package org.apache.guacamole.rest.history;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.apache.guacamole.GuacamoleException;
